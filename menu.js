f003badfe93fab6426c71303272642ecdf52272b
nguyenviethoa1A <nguyenviethoa2903@gmail.com>
2024-10-20 21:37:03 +0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,22 @@
+// script.js
+function toggleMenu() {
+    var submenu = document.getElementById("submenu");
+    if (submenu.classList.contains("show")) {
+        submenu.classList.remove("show");
+    } else {
+        submenu.classList.add("show");
+    }
+}
+
+// Đóng menu nếu người dùng click bên ngoài menu
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
